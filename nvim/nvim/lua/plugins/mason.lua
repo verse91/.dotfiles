fa7e30a34b436e363330e479139e84fef95776f5
VERSE <sunnyone0901@gmail.com>
2025-08-28 11:17:00 +0700

    update nvim config

---
+++ nvim/nvim/lua/plugins/mason.lua
@@ -0,0 +1,16 @@
+return {
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "nxls",
+        "goimports",
+        "gofumpt",
+        "gomodifytags",
+        "impl",
+        "delve",
+        "typescript-language-server",
+      },
+    },
+  },
+}
