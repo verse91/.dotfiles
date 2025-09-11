ea56df9ef8345872fda2afe87b3f79e989393084
verse91 <versedev.store@proton.me>
2025-09-11 20:20:57 +0700

    Auto update dotfiles

---
+++ config/nvim/lua/plugins/multi-cursor.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "mg979/vim-visual-multi",
+    branch = "master",
+    -- tùy chọn: keymaps hay config thêm
+    init = function()
+      -- ví dụ map lại phím nếu bạn muốn
+    end,
+  },
+}
